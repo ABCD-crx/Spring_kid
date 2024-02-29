f2a522921b319810e9449265ba0d0c368b0f8efe
ABCD <931710829@qq.com>
2024-02-29 22:47:01 +0800

    ICP备案信息优化

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-    <NavBar/>
+  <NavBar/>
+  <div id="v-content" v-bind:style="{minHeight: Height + 'px'}">
     <router-view />
+  </div>
   <Footer ></Footer>
 </template>
 
@@ -16,7 +18,18 @@
     Footer,
     },
 
-  }
+    data() {
+      return {
+       Height: 0
+     }
+    },
+    mounted(){
+      this.Height = document.documentElement.clientHeight - 120;  
+      window.onresize = ()=> {this.Height = document.documentElement.clientHeight - 120}
+    }
+    
+}
+  
 </script>
 
 <style>
