1425c8dce733295ed90ca68ce781cc9503e06539
ABCD <931710829@qq.com>
2024-03-12 15:02:47 +0800

    公安备案完成

--- web/src/components/FooterBar.vue
+++ web/src/components/FooterBar.vue
@@ -1,9 +1,13 @@
 <template>
      <p style="background:none;height: 5%;bottom: 0px;width: 100%;text-align: center;">
+        <a class="beian" style="color:#000;"
+            href="https://beian.miit.gov.cn/#/Integrated/index"
+            target="_blank">陕ICP备2024027990号&nbsp;&nbsp;
+        </a>
         <img style="padding-top:2px;" src="@/assets/images/police_beian.png">
-        <a class="banquan" style="color:#000;"
-        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=备案编号"
-        target="_blank">陕ICP备2024027990号</a>
+        <a class = "beian" style="color:#000;" 
+            href="https://beian.mps.gov.cn/#/query/webSearch?code=61011102000609" 
+            rel="noreferrer" target="_blank"> 陕公网安备61011102000609号</a>
     </p>
 </template>
 
@@ -12,4 +16,16 @@
 
 
 <style scoped>
+.beian:link{
+    color: black;
+}
+
+.beian:hover{
+    color: #409EFF;
+    text-decoration: underline; 
+}
+
+.beian:active {
+    color: orange;
+}
 </style>
